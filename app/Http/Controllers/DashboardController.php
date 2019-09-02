0672d3dd052bc454eae0cc3417ddb8690d7cc782
Tauhidul Alam <tauhid@Tauhiduls-MacBook-Pro.local>
2019-09-02 20:41:33 +0600

    Fixed issues in final v1

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,8 @@ use App\Cement;
 use App\CementBrand;
 use App\Rod;
 use App\RodBrand;
+use App\BankSaving;
+use App\DueCollection;
 use Carbon\Carbon;
 use DB;
 
@@ -42,17 +44,37 @@ class DashboardController extends Controller
                         ->whereMonth('created_at', '=', Carbon::now()->month)
                         ->whereNull('due_no')->get();
 
-
       $rodRecords = $this->process_chart_information($rodRecords);
 
-      $cementBrandRecords = CementBrand::where('active', 1)->get();
+      $bankSavingsRecords = BankSaving::select('id', 'bank_account_number',
+                        'amount', 'created_at')
+                        ->whereMonth('created_at', '=', Carbon::now()->month)
+                        ->get();
+
+
+      $bankSavingsRecords =
+      $this->process_bank_saving_chart_information($bankSavingsRecords);
+
+      $dueCollectionsSavingsRecords =
+      DueCollection::select('id',
+                        'amount', 'created_at')
+                        ->whereMonth('created_at', '=', Carbon::now()->month)
+                        ->get();
+
+
+      $dueCollectionsSavingsRecords =
+      $this->process_due_collection_chart_information($dueCollectionsSavingsRecords);
+
+      $cementBrandRecords = CementBrand::get();
       $cement_brands = $this->process_cement_brands($cementBrandRecords);
 
-      $rodBrandRecords = RodBrand::where('active', 1)->get();
+      $rodBrandRecords = RodBrand::get();
       $rod_brands = $this->process_rod_brands($rodBrandRecords);
 
       return view('dashboard')->with(['cement_records' => $cementRecords,
       'cement_brands' => $cement_brands,
+      'bank_saving_records' => $bankSavingsRecords,
+      'due_collection_records' => $dueCollectionsSavingsRecords,
       'rod_records' => $rodRecords,
       'rod_brands' => $rod_brands]);
     }
@@ -93,6 +115,50 @@ class DashboardController extends Controller
       return $brand_array;
     }
 
+    private function process_due_collection_chart_information($records){
+      if (!empty($records)){
+        foreach ($records as $key => $record) {
+          $label = Carbon::parse($record['created_at'])->format('d M');
+          if(isset($dueCollectionRecords) && array_key_exists($label, $dueCollectionRecords)){
+            $previous_data = $dueCollectionRecords[$label];
+            $dueCollectionRecords[$label] = $previous_data + $record['amount'];
+          } else {
+            $dueCollectionRecords[$label] = $record['amount'];
+          }
+        }
+
+        $chart_records['labels'] = isset($dueCollectionRecords) ? array_keys($dueCollectionRecords) : [];
+        $chart_records['data'] = isset($dueCollectionRecords) ? array_values($dueCollectionRecords) : [];
+        $chart_records['total_due_collections'] = isset($dueCollectionRecords) ?  array_sum($dueCollectionRecords) : 0;
+
+        return $chart_records;
+      }
+
+      return [];
+    }
+
+    private function process_bank_saving_chart_information($records){
+      if (!empty($records)){
+        foreach ($records as $key => $record) {
+          $label = Carbon::parse($record['created_at'])->format('d M');
+          if(isset($bankSavingRecords) && array_key_exists($label, $bankSavingRecords)){
+            $previous_data = $bankSavingRecords[$label];
+            $bankSavingRecords[$label] = $previous_data + $record['amount'];
+          } else {
+            $bankSavingRecords[$label] = $record['amount'];
+          }
+        }
+
+        $chart_records['labels'] = isset($bankSavingRecords) ? array_keys($bankSavingRecords) : [];
+        $chart_records['data'] = isset($bankSavingRecords) ? array_values($bankSavingRecords) : [];
+        $chart_records['total_savings'] = isset($bankSavingRecords) ? array_sum($bankSavingRecords) : 0;
+
+        return $chart_records;
+      }
+
+      return [];
+    }
+
     private function process_chart_information($records){
       if (!empty($records)){
         foreach ($records as $key => $record) {
@@ -113,12 +179,12 @@ class DashboardController extends Controller
           }
         }
 
-        $chart_records['labels'] = array_keys($sellRecords);
-        $chart_records['data'] = array_values($sellRecords);
-        $chart_records['total_sell'] = array_sum($sellRecords);
+        $chart_records['labels'] = isset($sellRecords) ? array_keys($sellRecords) : [];
+        $chart_records['data'] = isset($sellRecords) ? array_values($sellRecords) : [];
+        $chart_records['total_sell'] = isset($sellRecords) ? array_sum($sellRecords) : 0;
 
-        $chart_records['brands'] = array_keys($totalAmountRecords);
-        $chart_records['brands_data'] = array_values($totalAmountRecords);
+        $chart_records['brands'] = isset($totalAmountRecords) ? array_keys($totalAmountRecords) : [];
+        $chart_records['brands_data'] = isset($totalAmountRecords) ? array_values($totalAmountRecords) : [];
 
         return $chart_records;
       }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,10 +4,13 @@
 <div class="container">
     <div class="row justify-content-center">
       <div class="col-sm-6">
-        <div class="card">
+        <div class="card dashboard-card">
           <div class="card-body">
             <h5 class="card-title">সিমেন্ট বিক্রয়ের পরিসংখ্যান</h5>
-            <canvas id="cement-chart" width="400" height="200"></canvas>
+            @if(isset($cement_records))
+            <div class="canvas-chart">
+              <canvas id="cement-chart" width="400" height="200"></canvas>
+            </div>
             <script type="text/javascript">
 
               var ctx = document.getElementById('cement-chart').getContext('2d');
@@ -39,7 +42,7 @@
               }
           });
           </script>
-          <div>
+          <div class="chart-additional-information">
             @if(isset($cement_records['total_sell']) && $cement_records['total_sell'] != '')
             <p>মোট বিক্রয় {{ json_encode($cement_records['total_sell']) }} টাকা</p>
             @endif
@@ -49,14 +52,18 @@
               @endforeach
             @endif
           </div>
+          @endif
           </div>
         </div>
       </div>
       <div class="col-sm-6">
-        <div class="card">
+        <div class="card dashboard-card">
           <div class="card-body">
             <h5 class="card-title">রড বিক্রয়ের পরিসংখ্যান</h5>
-            <canvas id="rod-chart" width="400" height="200"></canvas>
+            @if(isset($rod_records))
+            <div class="canvas-chart">
+              <canvas id="rod-chart" width="400" height="200"></canvas>
+            </div>
             <script type="text/javascript">
 
               var ctx = document.getElementById('rod-chart').getContext('2d');
@@ -88,7 +95,7 @@
               }
           });
           </script>
-          <div>
+          <div class="chart-additional-information">
             @if(isset($rod_records['total_sell']) && $rod_records['total_sell'] != '')
             <p>মোট বিক্রয় {{ json_encode($rod_records['total_sell']) }} টাকা</p>
             @endif
@@ -98,40 +105,121 @@
               @endforeach
             @endif
           </div>
+          @endif
           </div>
         </div>
       </div>
     </div>
     <div class="row justify-content-center">
       <div class="col-sm-6">
-        <div class="card">
+        <div class="card dashboard-card">
           <div class="card-body">
-            <h5 class="card-title">ব্যাংকে জমার বিবরণ</h5>
-            <a href="#" class="btn btn-primary">ক্লিক করুন</a>
+            <h5 class="card-title">ব্যাংকে জমার পরিসংখ্যান</h5>
+            @if(isset($bank_saving_records))
+            <div class="canvas-chart">
+              <canvas id="bank-saving-chart" width="400" height="200"></canvas>
+            </div>
+            <script type="text/javascript">
+
+              var ctx = document.getElementById('bank-saving-chart').getContext('2d');
+              var myChart = new Chart(ctx, {
+              type: 'line',
+              data: {
+                  labels: {!! json_encode($bank_saving_records['labels']) !!},
+                  datasets: [{
+                      data: {!! json_encode($bank_saving_records['data']) !!},
+                      backgroundColor: 'rgba(47, 152, 208, 0.2)',
+                      borderColor: '#3490dc',
+                      borderWidth: 1
+                  }]
+              },
+              options: {
+                  tooltips: {
+                      enabled: true,
+                  },
+                  legend: {
+                      display: false
+                  },
+                  scales: {
+                      yAxes: [{
+                          ticks: {
+                              beginAtZero: true
+                          }
+                      }]
+                  }
+              }
+          });
+          </script>
+          <div class="chart-additional-information">
+            @if(isset($bank_saving_records['total_savings']) && $bank_saving_records['total_savings'] != '')
+            <p>মোট জমা {{ json_encode($bank_saving_records['total_savings']) }} টাকা</p>
+            @endif
+          </div>
+          @endif
           </div>
         </div>
       </div>
       <div class="col-sm-6">
-        <div class="card">
+        <div class="card dashboard-card">
           <div class="card-body">
-            <h5 class="card-title">দোকানের খরচ</h5>
-            <a href="{{ url('daily-costs') }}" class="btn btn-primary">ক্লিক করুন</a>
+            <h5 class="card-title">বাকি আদায়ের পরিসংখ্যান</h5>
+            @if(isset($due_collection_records))
+            <div class="canvas-chart">
+              <canvas id="due-collection-chart" width="400" height="200"></canvas>
+            </div>
+            <script type="text/javascript">
+
+              var ctx = document.getElementById('due-collection-chart').getContext('2d');
+              var myChart = new Chart(ctx, {
+              type: 'line',
+              data: {
+                  labels: {!! json_encode($due_collection_records['labels']) !!},
+                  datasets: [{
+                      data: {!! json_encode($due_collection_records['data']) !!},
+                      backgroundColor: 'rgba(47, 152, 208, 0.2)',
+                      borderColor: '#3490dc',
+                      borderWidth: 1
+                  }]
+              },
+              options: {
+                  tooltips: {
+                      enabled: true,
+                  },
+                  legend: {
+                      display: false
+                  },
+                  scales: {
+                      yAxes: [{
+                          ticks: {
+                              beginAtZero: true
+                          }
+                      }]
+                  }
+              }
+          });
+          </script>
+          <div class="chart-additional-information">
+            @if(isset($due_collection_records['total_due_collections']) && $due_collection_records['total_due_collections'] != '')
+            <p>মোট বাকি আদায় {{ json_encode($due_collection_records['total_due_collections']) }} টাকা</p>
+            @endif
+          </div>
+          @endif
           </div>
         </div>
       </div>
     </div>
-    @if(Auth::user()->hasAnyRole('admin'))
-    <div class="row justify-content-center">
-      <div class="col-sm-6">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">কর্মচারিদের বেতন</h5>
-            <a href="{{ url('salaries') }}" class="btn btn-primary">ক্লিক করুন</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-6"></div>
-    </div>
-    @endif
 </div>
+<style>
+  .chart-additional-information {
+    margin: 30px 0 0 10px;
+  }
+
+  .dashboard-card {
+    margin-bottom: 20px;
+  }
+
+  .canvas-chart {
+    margin: 30px 0;
+  }
+</style>
 @endsection
